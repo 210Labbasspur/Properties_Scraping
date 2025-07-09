791f5428ea0b89f9a4c832796927132e754aacaa
Syed Hassan Mujtaba Sherazi <syedhassanmujtabasherazi@gmail.com>
2025-07-09 13:04:00 +0500

    Shores_Rent - www.shores.com.au
    Scrapy Script to extract and save data into MongoDB, while following a data-format. Source = https://www.shores.com.au/properties-for-rent/

---
+++ Shores_Rent/Shores_Rent/pipelines.py
@@ -0,0 +1,26 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+from itemadapter import ItemAdapter
+# import pymongo
+
+
+class ShoresRentPipeline:
+
+    # def __init__(self):
+    #     self.conn = pymongo.MongoClient(
+    #         'localhost',
+    #         27017
+    #     )
+    #     db  = self.conn['Shores_Record']
+    #     self.collection = db['Shores_Rent']
+
+    def process_item(self, item, spider):
+        # self.collection.insert_one(dict(item))
+        # collection_name = spider.name
+        # self.db[collection_name].insert_one(dict(item))
+        return item
